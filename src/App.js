0ce32cd85fdf8e4160b494ac69fedd4e1b593f45
Subham Mani <manisubham09@gmail.com>
2023-12-27 18:53:55 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import "./App.css";
+import appStore from "./utils/appStore";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Body from "./components/Body";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+      children: [
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      Yoo! Reactive ToDo
-    </div>
+    <Provider store={appStore}>
+      <div className="App">
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
   );
 }
 
